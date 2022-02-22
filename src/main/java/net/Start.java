ce2272b7b9573147017fdc66948c686e8f83a02e
EnDrOLl_ <66158890+34663@users.noreply.github.com>
2022-02-22 17:05:18 +0900

    Initial commit

---
+++ src/main/java/net/Start.java
@@ -0,0 +1,17 @@
+package net;
+
+import java.util.Arrays;
+
+import net.minecraft.client.main.Main;
+
+public class Start {
+    public static void main(String[] args) {
+        Main.main(concat(new String[]{"--version", "1337", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.12", "--userProperties", "{}"}, args));
+    }
+
+    public static <T> T[] concat(T[] first, T[] second) {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+}
